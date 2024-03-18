9309e7fb37e43a57244377e2b8c460b4bd2c4f7b
alexgaarciia <al.garcia636@gmail.com>
2024-03-18 19:59:34 -0400

    added code and data

---
+++ utils.py
@@ -0,0 +1,19 @@
+from skimage import io
+from skimage.util import img_as_float
+from skimage.color import rgb2gray
+import numpy as np
+from scipy.ndimage import correlate
+import sklearn.cluster
+from scipy.spatial.distance import cdist
+
+def computeHistogram(img_file, F, textons):
+    
+    ### YOUR CODE HERE
+
+    ### END YOUR CODE
+    
+def createTextons(F, file_list, K):
+
+    ### YOUR CODE HERE
+
+    ### END YOUR CODE
